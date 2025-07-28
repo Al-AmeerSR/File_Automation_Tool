1e503385e514a8e0aa8ff63de25f884e930ef537
Al-AmeerSR <iamameer37@gmail.com>
2025-07-28 23:23:18 +0530

    code refactoring

--- File_Automation_Tool_Backend/src/main/java/com/automation_tool/constants/FAT_Constants.java
+++ File_Automation_Tool_Backend/src/main/java/com/automation_tool/constants/FAT_Constants.java
@@ -2,14 +2,14 @@ package com.automation_tool.constants;
 
 public class FAT_Constants {
 
-    public static String CRON_TRIGGER ="CRON";
-    public static String ONETIME_TRIGGER ="ONETIME";
-    public static String JOB_STATUS_CREATED ="CREATED";
-    public static String JOB_STATUS_RUNNING ="RUNNING";
-    public static String JOB_STATUS_FINISHED ="FINISHED";
-    public static String JOB_STATUS_FAILED ="FAILED";
-    public static String JOB_STATUS_PAUSED ="PAUSED";
-    public static String JOB_STATUS_RESUMED ="RESUMED";
-    public static String JOB_STATUS_UPDATED ="UPDATED"
+    public static final String CRON_TRIGGER ="CRON";
+    public static final String ONETIME_TRIGGER ="ONETIME";
+    public static final String JOB_STATUS_CREATED ="CREATED";
+    public static final String JOB_STATUS_RUNNING ="RUNNING";
+    public static final String JOB_STATUS_FINISHED ="FINISHED";
+    public static final String JOB_STATUS_FAILED ="FAILED";
+    public static final String JOB_STATUS_PAUSED ="PAUSED";
+    public static final String JOB_STATUS_RESUMED ="RESUMED";
+    public static final String JOB_STATUS_UPDATED ="UPDATED"
             ;
 }
