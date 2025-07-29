69d05a08350a735df56b8d3a16d528ebb425b30b
Al-AmeerSR <iamameer37@gmail.com>
2025-07-29 13:34:43 +0530

    prometheus and grafana dependencies added for monitoring

--- File_Automation_Tool_Backend/src/main/java/com/automation_tool/config/SecurityConfig.java
+++ File_Automation_Tool_Backend/src/main/java/com/automation_tool/config/SecurityConfig.java
@@ -44,7 +44,8 @@ public class SecurityConfig {
                                 "/api/v1/auth/refresh",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html",
+                                "/actuator/**"
                         ).permitAll()
                         .anyRequest().authenticated())
                 .formLogin(AbstractHttpConfigurer::disable)
